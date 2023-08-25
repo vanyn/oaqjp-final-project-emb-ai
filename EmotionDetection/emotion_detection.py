c8e428441435cc204613c58533099bd9741692bc
Tom Van <tomvan.dev@gmail.com>
2023-08-24 21:16:25 -0400

    take 1-5 completed of final project

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    if not text_to_analyze.strip():  # Check for empty or blank input
+        return None
+
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    
+    response = requests.post(url, json = myobj, headers=header)
+    
+    if response.status_code == 200:
+
+        formatted_response = json.loads(response.text)
+        emotion_predictions = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotion_predictions.items(), key=lambda x: x[1])[0]
+
+        return {
+            'anger': emotion_predictions['anger'],
+            'disgust': emotion_predictions['disgust'],
+            'fear': emotion_predictions['fear'],
+            'joy': emotion_predictions['joy'],
+            'sadness': emotion_predictions['sadness'],
+            'dominant_emotion': dominant_emotion
+        }
+    else:
+        return None
+
+
+# python3.11 cmds ::
+# from emotion_detection import emotion_detector
+# emotion_detector("I am so happy I am doing this")
+
+# from EmotionDetection.emotion_detection import emotion_detector
